0c92d65093f5f0be2ad29aa5ba35c6761f3640d6
Lucas Trabuchi <trabu@MacBook-Pro-de-Lucas.local>
2022-01-30 20:13:42 -0300

    class and function to create first block

---
+++ blockchain.py
@@ -0,0 +1,15 @@
+# Module 1 - Create Blockchain
+
+
+import datetime
+import haslib
+import json
+from flask import Flas, jsonify
+
+# Part 1 - Building a Blockchain
+
+class Blockchain:
+        
+    
+
+#Part 2 - Mining our Blockchain
